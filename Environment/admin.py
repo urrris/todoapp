51e099cd9c0b5185dc0c0e7db87fea0417e4976a
Pavel <urrisend@gmail.com>
2024-03-17 19:09:22 +0300

    Adding environment models (User, Project, Task and Subtask) to the application

--- Environment/admin.py
+++ Environment/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import User, Project, Task, Subtask
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email"]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "creator"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["title", "project"]
+
+
+@admin.register(Subtask)
+class Subtask(admin.ModelAdmin):
+    list_display = ["title", "task"]
--- Environment/models.py
+++ Environment/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField('Псевдоним', max_length=50)
+    email = models.EmailField('E-mail')
+    password = models.CharField('Пароль', max_length=50)
+    height = models.IntegerField("Высота фото", default=0)
+    width = models.IntegerField("Ширина фото", default=0)
+    photo = models.ImageField('Фото', blank=True, height_field="height", width_field="width", upload_to=f"users/{email}/photos")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f"User({self.username}: {self.email})"
+
+
+class Project(models.Model):
+    title = models.CharField('Название', max_length=50)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="own_projects")
+    coworkers = models.ManyToManyField(User, related_name="projects")
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
+    def __str__(self):
+        return f"Project({self.title}: {self.creator})"
+
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    description = models.TextField("Описание", blank=True)
+    priority = models.IntegerField("Приоритет", choices=[(1, "High"), (2, "Middle"), (3, "Low")], default=1)
+    deadline = models.DateField('Дата закрытия задачи')
+    status = models.CharField(choices={
+        "Todo": "Todo",
+        "InProgress": "In progress",
+        "Done": "Done"
+    }, default="Todo")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="own_tasks")
+    coworkers = models.ManyToManyField(User, related_name="tasks")
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+    def __str__(self):
+        return f"Task({self.title}: {self.project})"
+
+
+class Subtask(models.Model):
+    title = models.CharField('Название', max_length=35)
+    description = models.TextField("Описание", blank=True)
+    priority = models.IntegerField("Приоритет", choices=[(1, "High"), (2, "Middle"), (3, "Low")], default=1)
+    status = models.CharField(choices={
+        "Todo": "Todo",
+        "Done": "Done"
+    }, default="Todo")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="subtasks")
+
+    class Meta:
+        verbose_name = "Подзадача"
+        verbose_name_plural = "Подзадачи"
+
+    def __str__(self):
+        return f"Subtask({self.title}: {self.task})"
